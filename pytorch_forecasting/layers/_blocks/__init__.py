6390a5b35e94f75c099796f4854408e3c659dc93
Seaic Mac Murchadha <seaicmacmurchadha@yahoo.com>
2026-09-04 01:14:02 +0100

    Move TSMixer block to shared blocks

--- pytorch_forecasting/layers/_blocks/__init__.py
+++ pytorch_forecasting/layers/_blocks/__init__.py
@@ -3,9 +3,11 @@ from pytorch_forecasting.layers._blocks._scinet_block import SCIBlock
 from pytorch_forecasting.layers._blocks._softs_block import (
     STADModule,
 )
+from pytorch_forecasting.layers._blocks._tsmixer_block import TSMixerBlock
 
 __all__ = [
     "ResidualBlock",
     "SCIBlock",
     "STADModule",
+    "TSMixerBlock",
 ]
